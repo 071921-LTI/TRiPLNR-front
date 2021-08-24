082fcc1cb6b6da2830b7cd9f32bd3ab8d71fa0f1
dvellerlti <david.veller@lntinfotech.com>
2021-08-24 16:21:41 -0400

    added authservice, kinda works

--- triplnr/src/app/services/auth-service.service.ts
+++ triplnr/src/app/services/auth-service.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(user: User): Observable<String>{
+    return this.http.post("http://localhost:8080/triplnr/auth/login", user).pipe(
+      map(response => response as String));
+  }
 }
