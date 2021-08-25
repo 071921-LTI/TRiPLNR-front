c123c7a4e5cf9138ab56aa289feb1152766656b3
dvellerlti <david.veller@lntinfotech.com>
2021-08-25 11:47:13 -0400

    changed user model, added address to registration

--- triplnr/src/app/models/user.ts
+++ triplnr/src/app/models/user.ts
@@ -1,28 +1,14 @@
-import { first } from "rxjs/operators";
-
-export class User {
-
-    constructor(userId:number, username:String, password:String, firstName:String, lastName:String) { 
-        this.userId = userId;
-        this.username = username;
-        this.password = password;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = null;
-        this.trips = null;
-    }
+export interface User {
 
     
 
-    userId : number;
-    username : String;
-    password : String;
-    firstName : String;
-    lastName : String;
-    //address
-    address : null;
-    //trips
-    trips : null;
+    userId?: number;
+    username?: String;
+    password?: String;
+    firstName?: String;
+    lastName?: String;
+    address?: null;
+    trips?: null;
 
 
 
