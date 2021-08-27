b9f8e174dce0840d35ca3a665553c6328785fb3c
Chris Pantani <chris.pantani@lntinfotech.com>
2021-08-27 11:10:28 -0400

    Authorization header in need of testing

--- triplnr/src/app/components/create-trip/create-trip.component.ts
+++ triplnr/src/app/components/create-trip/create-trip.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Timestamp } from 'rxjs/internal/operators/timestamp';
 import { TripServiceService } from 'src/app/services/trip-service.service';
+import { Trip } from 'src/app/models/trip'
 
 @Component({
   selector: 'app-create-trip',
@@ -13,9 +13,32 @@ export class CreateTripComponent implements OnInit {
   constructor(private tripService: TripServiceService, private router:Router) { }
 
   destination: String = '';
-  origin: String = '';
   tripName: String = '';
-  startTime: String = '';
+  manager: String = '';
+  error: String = '';
+
+  trip?:Trip;
+  
+  token = sessionStorage.getItem("token");
+  //either pass token through with trip data or 
+  //add new header
+
+
+
+  createTrip(): void {
+    this.trip = {
+      destination: this.destination,
+      tripName: this.tripName
+    }
+    this.tripService.create(this.trip).subscribe(
+      response => {
+        if(response != null){
+          this.router.navigate(['/trip-dashboard']);
+        } else {
+        this.error = "Trip Creation Error"
+      }
+    )
+  }
 
   ngOnInit(): void {
   }
--- triplnr/src/app/services/trip-service.service.ts
+++ triplnr/src/app/services/trip-service.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Trip } from '../models/trip';
 
+let token = sessionStorage.getItem("token");
+console.log("token");
+
+let header = new HttpHeaders();
+if(token != null){
+  header.set('Authorization', token)
+}
+
 @Injectable({
   providedIn: 'root'
 })
-
-
+ 
 export class TripServiceService {
 
-  constructor(private http: HttpClient) { }
-
+  constructor(private http: HttpClient, private headers: HttpHeaders) { }
+  
   create(trip: Trip): Observable<String>{
-    return this.http.post("http://localhost:8080/triplnr/trip/create", trip).pipe(
+    return this.http.post("http://localhost:8080/triplnr/trip/create", trip, {
+      headers: header}).pipe(
       map(response => response as String));
   }  
 
