c5fe0dcd85e1bca682456ea32a1c91c46574b50b
Michael Wilder <87658901+Michael-Wilder00@users.noreply.github.com>
2021-08-27 17:04:07 -0400

    Add files via upload

--- triplnr/src/app/services/trip-service.service.ts
+++ triplnr/src/app/services/trip-service.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Trip } from '../models/trip';
+import { User } from '../models/user';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -13,9 +16,27 @@ export class TripServiceService {
 
   constructor(private http: HttpClient) { }
 
-  create(trip: Trip): Observable<String>{
-    return this.http.post("http://localhost:8080/triplnr/trip/create", trip).pipe(
+  create(trip: Trip, token:string): Observable<String>{
+    console.log("in create in service");
+    let headers = new HttpHeaders({
+      Authorization: token
+    });
+    
+    //headers.set('Authorization', token);
+    console.log(headers);
+
+    return this.http.post("http://localhost:8080/triplnr/trip/create", trip, {headers}).pipe(
       map(response => response as String));
   }  
+  getTrips(token: string): Observable<Trip[]>{
+    console.log("in getTrips in service");
+    let headers = new HttpHeaders({
+      Authorization: token
+    });
+    console.log(headers);
+    return this.http.get<Trip[]>(environment.tripURL + "dashboard", {headers}).pipe(
+      map(response => response as Trip[]));
+  }
+  
 
 }
