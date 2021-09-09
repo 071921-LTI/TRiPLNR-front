76edc1eb97caa68f3837e8f9dfc7fb309dfc9931
dvellerlti <david.veller@lntinfotech.com>
2021-09-09 10:57:50 -0400

    changed env

--- triplnr/src/environments/environment.prod.ts
+++ triplnr/src/environments/environment.prod.ts
@@ -4,5 +4,7 @@ export const environment = {
   authURL: "http://ec2-18-216-192-115.us-east-2.compute.amazonaws.com:8081/auth/",
   userURL: "http://ec2-18-216-192-115.us-east-2.compute.amazonaws.com:8081/users/",
   tripURL: "http://ec2-18-216-192-115.us-east-2.compute.amazonaws.com:8081/trip/",
-  requestURL: "http://ec2-18-216-192-115.us-east-2.compute.amazonaws.com:8081/friends/"
+  requestURL: "http://ec2-18-216-192-115.us-east-2.compute.amazonaws.com:8081/friends/",
+  //api key for Google Maps api
+  mapsKey: "AIzaSyBdMa5mObr7e3k11wJ3AxhCRenQ9EnOeKw"
 };
