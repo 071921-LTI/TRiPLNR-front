be61444d9c925056ab99bf413140afcd6297ec45
Enrique Gomez <enrique_g_gomez@hotmail.com>
2021-09-09 08:17:19 -0500

    Update environment.ts

--- triplnr/src/environments/environment.ts
+++ triplnr/src/environments/environment.ts
@@ -1,11 +1,4 @@
 
 export const environment = {
-    production: false,
-    authURL: "http://localhost:8080/auth/",
-    userURL: "http://localhost:8080/users/",
-    tripURL: "http://localhost:8080/trip/",
-    requestURL: "http://localhost:8080/friends/",
-    mapsKey: "AIzaSyBdMa5mObr7e3k11wJ3AxhCRenQ9EnOeKw",
-    AUTH_DOMAIN: "dev-hd772wgr.us.auth0.com",
-    AUTH_CLIENT_ID: "yqM7cxHvnHKDXi1Nd1hjqzapkQe1de2g"
+    production: false
 };
