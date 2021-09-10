9abceafe54f6d129c208e2f3b42286704f46c931
dvellerlti <david.veller@lntinfotech.com>
2021-09-10 14:18:16 -0400

    refactor for auth0

--- triplnr/src/app/components/register/register.component.ts
+++ triplnr/src/app/components/register/register.component.ts
@@ -4,6 +4,7 @@ import { AuthServiceService } from 'src/app/services/auth-service.service';
 import { AddressFormComponent } from '../address-form/address-form.component';
 import { Router } from '@angular/router';
 import { Auth0ServiceService } from 'src/app/services/auth0-service.service';
+import { UserServiceService } from 'src/app/services/user-service.service';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +13,7 @@ import { Auth0ServiceService } from 'src/app/services/auth0-service.service';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private authService : AuthServiceService, private router:Router, private auth0: Auth0ServiceService) { }
+  constructor(private userService:UserServiceService, private router:Router, private auth0: Auth0ServiceService) { }
 
   
   stateArr = ['CT', 'GA', 'NY', 'TX', 'VT'];
@@ -51,10 +52,10 @@ export class RegisterComponent implements OnInit {
       address: this.address
     }
     //calls authService register method passes through new user object
-    this.authService.register(this.user).subscribe(
+    this.userService.createUser(this.user).subscribe(
       (response) => {
         //sets token from header
-        this.token = response.headers.get("Authorization") || '';
+        this.token = this.sub || '';
         if (this.token != null && this.token != ''){
         //checks token and saves token in sessoion for later use if not null or empty
         sessionStorage.setItem("token", this.token.valueOf());
