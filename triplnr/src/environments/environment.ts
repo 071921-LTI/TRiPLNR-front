01c6942c9e0bff5398abd69304e56ccead335121
dvellerlti <david.veller@lntinfotech.com>
2021-09-17 14:54:08 -0400

    fixed some bugs

--- triplnr/src/environments/environment.ts
+++ triplnr/src/environments/environment.ts
@@ -5,6 +5,7 @@ export const environment = {
     userURL: "http://localhost:8080/users/",
     tripURL: "http://localhost:8080/trip/",
     requestURL: "http://localhost:8080/friends/",
+    weatherURL: "http://localhost:8080/weather/",
     mapsKey: process.env.MAPS_KEY,
     AUTH_DOMAIN: process.env.AUTH_DOMAIN,
     AUTH_CLIENT_ID: process.env.AUTH_CLIENT_ID
